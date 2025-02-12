0531e5b6f5b04608881ae25f86834734e3c79d82
Guilherme Xavier <xguilherme1@gmail.com>
2025-02-12 16:19:46 -0300

    build

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,7 @@ import launcherPointsRouter from './routes/launcherPoints.routes';
 const app = express();
 const host = process.env.HOST || 'localhost';
 const port = process.env.NODE_PORT || '3333'
+
 app.use(express.json({ limit: '10mb' }));
 app.use(cors());
 app.use(usersRouter)
